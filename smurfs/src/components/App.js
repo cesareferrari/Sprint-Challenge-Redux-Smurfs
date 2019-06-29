1250196baba88055f6cb50ec4fc103c886507aa9
Cesare Ferrari <cesare@maryandferrari.com>
2019-06-29 13:48:09 -0400

    Get smurf list

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -7,6 +9,11 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,9 +21,20 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+
+      {this.props.smurfs.map(smurf => smurf.name)}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    smurfs: state.smurfs,
+    error: state.error,
+    fetchingSmurfs: state.fetchingSmurfs
+  }
+}
+
+export default connect(mapStateToProps, {getSmurfs})(App);
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,12 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+import { 
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAILURE
+} from '../actions';
+
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -33,6 +39,25 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case FETCH_SMURFS_START:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: ''
+      }
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: '',
+        smurfs: [ ...state.smurfs, ...action.payload ]
+      }
+    case FETCH_SMURFS_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      }
     default:
       return state
   }
